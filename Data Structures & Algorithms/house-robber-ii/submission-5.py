e9dd5e90a240ab93038183e7b36a1f4b2d20878d
William Bo <william.bo@mail.utoronto.ca>
2026-09-22 15:04:00 -0400

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+            
+        def robRange(lo, hi):
+            house2 = house1 = 0
+
+            for i in range(lo, hi):
+                house2, house1 = house1, max(house1, house2 + nums[i]) #either two previous house, and the next one, or just the previous house
+
+            return house1
+        
+        return max(robRange(0, len(nums) - 1), robRange(1, len(nums)))
+        
